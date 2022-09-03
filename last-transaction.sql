a7dbce7c86bca6a42e20d39e382b884109cfb37a
Monika Sharma <monika.pu@gmail.com>
2022-09-02 20:53:15 -0400

    ROW_NUMBER example

--- last-transaction.sql
+++ last-transaction.sql
@@ -1,3 +1,35 @@
 """
+Given a table of bank transactions with columns id, transaction_value, and created_at representing 
+the date and time for each transaction, write a query to get the last transaction for each day.
+
+The output should include the id of the transaction, datetime of the transaction, and the transaction 
+amount. Order the transactions by datetime.
+
+Example:
+
+Input:
+
+bank_transactions table
+
+Column	Type
+id	INTEGER
+created_at	DATETIME
+transaction_value	FLOAT
+Output:
+
+Column	Type
+created_at	DATETIME
+transaction_value	FLOAT
+id	INTEGER
+
+I need help
 
 """
+
+WITH timed_transactions AS 
+       (
+	SELECT * , ROW_NUMBER() OVER (PARTITION BY DATE(created_at) ORDER BY created_at DESC) as ordered_time
+	FROM bank_transactions
+	)
+SELECT  created_at,transaction_value, id  FROM timed_transactions
+WHERE ordered_time = 1;
